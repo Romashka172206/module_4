76f62e02ea0529e41484b9e176baea25febceee6
Romashka172206 <136432515+Romashka172206@users.noreply.github.com>
2023-06-18 11:04:52 +0300

    Delete сайт.py

--- сайт.py
+++
@@ -1,7 +0,0 @@
-def palindrome(s):
-    return s[::-1] == s
-
-while True:
-    s = input("введите слово: ")
-    print(f"{s} это слово является палиндромом" if
-    palindrome(s) else "это слово не является палиндромом")
