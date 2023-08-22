b8b8e15dbf210598653be5a4f39345be3174b44c
Romashka172206 <136432515+Romashka172206@users.noreply.github.com>
2023-08-22 17:31:10 +0300

    Delete gavmay directory

--- gavmay/app_advertisemen/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, top_sellers
-# from .views import lesson
-
-urlpatterns = [
-    path("",index, name="main-page"),
-    path('top-sellers/',top_sellers,name="top-sellers")
-    # path('lesson_4/',lesson)
-]
